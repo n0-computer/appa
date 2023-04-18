5350069a490c1c1229116883c5b5c541090390cd
dignifiedquire <me@dignifiedquire.com>
2023-04-18 14:17:26 +0200

    feat: add flatfs store

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use anyhow::Result;
+
+fn main() -> Result<()> {
     println!("Hello, world!");
+
+    let dir = "appa-store";
+    let store = appa::store::Store::new(dir)?;
+
+    store.put("hello", b"world")?;
+    let res = store.get("hello")?;
+    let path = store.get_path("hello")?;
+    println!(
+        "got hello => {}, stored in {}",
+        std::str::from_utf8(&res).unwrap(),
+        path.display()
+    );
+
+    Ok(())
 }
---
+++ src/store.rs
@@ -0,0 +1,5 @@
+mod flatfs;
+mod shard;
+
+pub use self::flatfs::Flatfs as Store;
+pub use self::shard::Shard;
