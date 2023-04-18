dd682b487b73d0390a68c1a01a178c0381005b2b
dignifiedquire <me@dignifiedquire.com>
2023-04-18 15:17:56 +0200

    main: use wnfs example to store wnfs data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::rc::Rc;
+
 use anyhow::Result;
-use wnfs::common::BlockStore;
+use chrono::Utc;
+use wnfs::{libipld::Cid, public::PublicDirectory};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -7,20 +10,43 @@ async fn main() -> Result<()> {
 
     let dir = "appa-store";
     let mut store = appa::store::Store::new(dir)?;
-    let data = b"hello world";
 
-    let cid = store
-        .put_block(data.to_vec(), libipld::IpldCodec::Raw)
+    // Create a new directory.
+    let root_dir = &mut Rc::new(PublicDirectory::new(Utc::now()));
+
+    // Add a /pictures/cats subdirectory.
+    root_dir
+        .mkdir(&["pictures".into(), "cats".into()], Utc::now(), &store)
         .await?;
-    println!("stored data under {}", cid);
 
-    let res = store.get_block(&cid).await?;
-    let path = store.get_path(&cid.to_string())?;
+    // Add a file to /pictures/dogs directory.
+    root_dir
+        .write(
+            &["pictures".into(), "dogs".into(), "billie.jpeg".into()],
+            Cid::default(),
+            Utc::now(),
+            &store,
+        )
+        .await?;
+
+    // Delete /pictures/cats directory.
+    root_dir
+        .rm(&["pictures".into(), "cats".into()], &store)
+        .await?;
+
+    // List all the children of /pictures directory.
+    let result = root_dir.ls(&["pictures".into()], &store).await?;
+
+    // Print the result.
+    println!("Files in /pictures: {:#?}", result);
+
+    // Store
+    let root_cid = root_dir.store(&mut store).await?;
 
     println!(
-        "got hello => {}, stored in {}",
-        std::str::from_utf8(&res).unwrap(),
-        path.display()
+        "store under {} at {}",
+        root_cid,
+        store.get_path(&root_cid.to_string())?.display()
     );
 
     Ok(())
