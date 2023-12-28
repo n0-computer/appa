908c1c948f33279e2c386612e4917c3efba7ab60
Philipp Krüger <philipp.krueger1@gmail.com>
2023-12-28 14:32:39 +0100

    Don't mind me deleting all of this :eyes:

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,1 @@
-pub mod fs;
-pub mod hash_manifest;
 pub mod store;
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-use std::sync::Arc;
-use std::{net::SocketAddr, str::FromStr};
+use std::io::Cursor;
+use std::path::{Component, Path};
 
-use anyhow::{Context as _, Result};
-use appa::fs::Fs;
-use appa::{hash_manifest::HashManifest, store::Store};
-use bytes::Bytes;
-use futures::FutureExt;
-use iroh::provider::DataSource;
-use iroh::{
-    porcelain::provide,
-    protocol::GetRequest,
-    provider::{create_collection, CustomHandler, Database},
-    PeerId,
-};
+use anyhow::{anyhow, Context as _, Result};
+use appa::store::flatfs::Flatfs;
+use cid::Cid;
+use futures::TryStreamExt;
 use tokio::io::AsyncWriteExt;
 
 use clap::{Parser, Subcommand};
 use tracing_subscriber::{prelude::*, EnvFilter};
+use wnfs::private::AccessKey;
+use wnfs::root_tree::RootTree;
 
 #[derive(Debug, Parser)]
 #[command(name = "appa")]
@@ -25,6 +19,15 @@ struct Cli {
     #[command(subcommand)]
     command: Commands,
 }
+
+/// Appa data directory
+const ROOT_DIR: &str = ".appa";
+
+/// Key storing the latest data root CID
+pub const DATA_ROOT: &str = "DATA_ROOT_V1";
+
+/// Key storing the access symmetric key for the private root directory
+pub const PRIVATE_ACCESS_KEY: &str = "PRIVATE_ACCESS_KEY_V1";
 
 #[derive(Debug, Subcommand)]
 enum Commands {
@@ -65,28 +68,6 @@ enum Commands {
         #[arg(value_name = "DESTINATION")]
         dest: String,
     },
-    /// Print a manifest
-    Manifest,
-    /// Pull any missing files from the node
-    Pull {
-        #[clap(long, short)]
-        peer: PeerId,
-        /// The authentication token to present to the server.
-        #[clap(long)]
-        auth_token: String,
-        /// Optional address of the provider, defaults to 127.0.0.1:4433.
-        #[clap(long, short)]
-        addr: Option<SocketAddr>,
-    },
-    /// Provide
-    Provide {
-        #[clap(long, short)]
-        addr: Option<SocketAddr>,
-        /// Auth token, defaults to random generated.
-        #[clap(long)]
-        auth_token: Option<String>,
-    },
-
     /// Import a file tree
     Import {
         /// Source directory (on your machine)
@@ -108,194 +89,140 @@ async fn main() -> Result<()> {
 
     let args = Cli::parse();
 
-    const ROOT_DIR: &str = ".appa";
-
     match args.command {
         Commands::Init => {
             println!("Initializing ...");
-            Fs::init(&ROOT_DIR).await?;
+            let mut root_tree = RootTree::empty(Flatfs::new(ROOT_DIR)?);
+            let access_key = root_tree.create_private_root(&["private".into()]).await?;
+            root_tree
+                .store
+                .put(PRIVATE_ACCESS_KEY, access_key.to_bytes()?)?;
+            commit_fs(root_tree).await?;
         }
         Commands::Mkdir { dir } => {
-            let mut fs = Fs::load(&ROOT_DIR).await?;
-            fs.mkdir(dir).await?;
-            fs.commit().await?;
+            let mut fs = load_fs().await?;
+            fs.mkdir(&parse_path(dir)).await?;
+            commit_fs(fs).await?;
         }
         Commands::Add { path, content } => {
-            let mut fs = Fs::load(&ROOT_DIR).await?;
-            fs.add(path, content).await?;
-            fs.commit().await?;
+            let mut fs = load_fs().await?;
+            fs.write(&parse_path(path), content.into_bytes()).await?;
+            commit_fs(fs).await?;
         }
         Commands::Rm { path } => {
-            let mut fs = Fs::load(&ROOT_DIR).await?;
-            fs.rm(path).await?;
-            fs.commit().await?;
+            let mut fs = load_fs().await?;
+            fs.rm(&parse_path(path)).await?;
+            commit_fs(fs).await?;
         }
         Commands::Ls { path } => {
-            let fs = Fs::load(&ROOT_DIR).await.context("load")?;
-            let list = fs.ls(path).await?;
+            let fs = load_fs().await.context("load")?;
+            let list = fs.ls(&parse_path(path)).await?;
             for (el, _) in list {
                 println!("{}", el);
             }
         }
         Commands::Cat { path } => {
-            let fs = Fs::load(&ROOT_DIR).await?;
-            let content = fs.cat(path).await?;
+            let fs = load_fs().await?;
+            let content = fs.read(&parse_path(path)).await?;
             tokio::io::stdout().write_all(&content).await?;
         }
         Commands::Mv { source, dest } => {
-            let mut fs = Fs::load(&ROOT_DIR).await?;
-            fs.mv(source, dest).await?;
-            fs.commit().await?;
+            let mut fs = load_fs().await?;
+            fs.basic_mv(&parse_path(source), &parse_path(dest)).await?;
+            commit_fs(fs).await?;
         }
-        Commands::Manifest => {
-            let fs = Fs::load(&ROOT_DIR).await?;
-            let manifest = fs.manifest_public()?;
-            println!("public manifest: {manifest:#?}");
-
-            let manifest = fs.manifest_private()?;
-            println!("private manifest: {manifest:#?}");
-        }
-        Commands::Pull {
-            peer,
-            addr,
-            auth_token,
-        } => {
-            let fs = Fs::load(&ROOT_DIR).await?;
-            let manifest = fs.manifest()?;
-            let store = fs.store().clone();
-            // pull hashes from store & put hashes in a vec
-            let mut opts = iroh::get::Options {
-                peer_id: Some(peer),
-                ..Default::default()
-            };
-            if let Some(addr) = addr {
-                opts.addr = addr;
-            };
-            let token = iroh::protocol::AuthToken::from_str(&auth_token)
-                .context("Wrong format for authentication token")?;
-
-            let buf = postcard::to_stdvec(&manifest)?;
-            tokio::select! {
-                biased;
-                _ = tokio::signal::ctrl_c() => {
-                    println!("interupting pull");
-                }
-                res = iroh::get::run(
-                    Bytes::from(buf).into(),
-                    token,
-                    opts,
-                    || async move { Ok(()) },
-                    move |mut data| {
-                        let store = store.clone();
-                        async move {
-                            if data.is_root() {
-                                let hash = data.request().name;
-                                let collection = data.read_collection(hash).await?;
-                                data.set_limit(collection.total_entries() + 1);
-                                data.user = Some(collection);
-                            } else {
-                                let index = usize::try_from(data.offset() - 1)?;
-                                let hash = data.user.as_ref().unwrap().blobs()[index].hash;
-                                let name = &data.user.as_ref().unwrap().blobs()[index].name;
-                                let key = if name == appa::fs::LATEST {
-                                    name.into()
-                                } else {
-                                    Store::key_for_hash(hash.as_ref())
-                                };
-                                let content = data.read_blob(hash).await?;
-                                tracing::debug!("received blob for hash {hash:?}");
-                                tokio::task::spawn_blocking(move || {
-                                    store.put(&key, content)
-                                }).await??;
-                            }
-                            data.end()
-                        }
-                    },
-                    None,
-                ) => {
-                    res?;
-                }
-            };
-        }
-        Commands::Provide { addr, auth_token } => {
-            let fs = Fs::load(&ROOT_DIR).await?;
-            let manifest = fs.manifest()?;
-
-            let db = Database::default();
-            let custom_handler = CollectionMirrorHandler {
-                manifest: Arc::new(manifest),
-                store: fs.store().clone(),
-            };
-            let provider = provide(
-                db.clone(),
-                addr,
-                auth_token,
-                None,
-                false,
-                None,
-                custom_handler,
-            )
-            .await?;
-
-            iroh::porcelain::display_provider_info(&provider)?;
-
-            let provider2 = provider.clone();
-            tokio::select! {
-                biased;
-                _ = tokio::signal::ctrl_c() => {
-                    println!("Shutting down provider...");
-                    provider2.shutdown();
-                }
-                res = provider => {
-                    res?;
+        Commands::Import { source, target } => {
+            let mut fs = load_fs().await?;
+            let target_path = parse_path(target.clone());
+            let mut files = futures::stream::iter(walkdir::WalkDir::new(&source));
+            while let Some(file) = files.try_next().await? {
+                let full_path = file.path();
+                println!("Importing {}...", full_path.to_string_lossy());
+                let rel_path = canonicalize_path(full_path.strip_prefix(&source)?)?;
+                let path = [target_path.clone(), parse_path(rel_path)].concat();
+                if file.file_type().is_dir() {
+                    fs.mkdir(&path).await?;
+                } else if file.file_type().is_file() {
+                    let content = tokio::fs::read(full_path).await?;
+                    // let size = content.len();
+                    fs.write(&path, content).await?;
                 }
             }
-        }
-
-        Commands::Import { source, target } => {
-            let mut fs = Fs::load(&ROOT_DIR).await?;
-            fs.import(&source, &target).await?;
-            fs.commit().await?;
-            println!("Imported {source} to {target}");
+            println!("Imported {source} to {target}, committing...");
+            commit_fs(fs).await?;
+            println!("Committed.")
         }
     }
 
     Ok(())
 }
 
-#[derive(Clone, Debug)]
-struct CollectionMirrorHandler {
-    manifest: Arc<HashManifest>,
-    store: Store,
+fn read_data_root(store: &Flatfs) -> Result<Cid> {
+    Ok(Cid::read_bytes(Cursor::new(
+        store.get(DATA_ROOT)?.ok_or(anyhow!("No data root"))?,
+    ))?)
 }
 
-impl CustomHandler for CollectionMirrorHandler {
-    fn handle(
-        &self,
-        data: Bytes,
-        database: &Database,
-    ) -> futures::future::BoxFuture<'static, anyhow::Result<GetRequest>> {
-        let database = database.clone();
-        let this = self.clone();
+fn commit_data_root(store: &Flatfs, cid: Cid) -> Result<()> {
+    store.put(DATA_ROOT, cid.to_bytes())?;
+    Ok(())
+}
 
-        async move {
-            let requestor_manifest: HashManifest = postcard::from_bytes(&data)?;
-            let diff = this.manifest.without(&requestor_manifest);
-            let mut sources = diff.to_sources(&this.store)?;
+fn read_access_key(store: &Flatfs) -> Result<AccessKey> {
+    Ok(AccessKey::parse(
+        store
+            .get(PRIVATE_ACCESS_KEY)?
+            .ok_or(anyhow!("No access key"))?,
+    )?)
+}
 
-            let path = this.store.get_path(appa::fs::LATEST)?;
-            sources.push(DataSource::NamedFile {
-                name: appa::fs::LATEST.into(),
-                path,
-            });
+fn parse_path(path: String) -> Vec<String> {
+    path.split('/')
+        .filter(|s| !s.is_empty())
+        .map(|s| s.to_string())
+        .collect()
+}
 
-            let (new_db, hash) = create_collection(sources).await?;
-            let new_db = new_db.to_inner();
-            database.union_with(new_db);
-            let request = GetRequest::all(hash);
-            println!("{:?}", request);
-            Ok(request)
-        }
-        .boxed()
-    }
+async fn load_fs() -> Result<RootTree<Flatfs>> {
+    let bs = Flatfs::new(ROOT_DIR)?;
+    let data_root = read_data_root(&bs).context("reading data root")?;
+    let mut root_tree = RootTree::load(&data_root, bs)
+        .await
+        .context("loading root tree")?;
+    let access_key = read_access_key(&root_tree.store)?;
+    root_tree
+        .load_private_root(&["private".into()], &access_key)
+        .await?;
+    Ok(root_tree)
+}
+
+async fn commit_fs(mut root_tree: RootTree<Flatfs>) -> Result<()> {
+    let cid = root_tree.store().await?;
+    commit_data_root(&root_tree.store, cid)?;
+    Ok(())
+}
+/// converts a canonicalized relative path to a string, returning an error if
+/// the path is not valid unicode
+///
+/// this will also fail if the path is non canonical, i.e. contains `..` or `.`,
+/// or if the path components contain any windows or unix path separators
+fn canonicalize_path(path: impl AsRef<Path>) -> anyhow::Result<String> {
+    let parts = path
+        .as_ref()
+        .components()
+        .map(|c| {
+            let c = if let Component::Normal(x) = c {
+                x.to_str().context("invalid character in path")?
+            } else {
+                anyhow::bail!("invalid path component {:?}", c)
+            };
+            anyhow::ensure!(
+                !c.contains('/') && !c.contains('\\'),
+                "invalid path component {:?}",
+                c
+            );
+            Ok(c)
+        })
+        .collect::<anyhow::Result<Vec<_>>>()?;
+    Ok(parts.join("/"))
 }
